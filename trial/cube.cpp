67b887814a9d48aab8d372da4266ff702000535f
jimin kim <0305jimin@gmail.com>
2025-03-07 11:01:16 +0900

    cube.cpp: using variable for scaling unit instead of using specific value

---
+++ trial/cube.cpp
@@ -0,0 +1,167 @@
+// Includes
+#include <stdio.h>
+#include <stdarg.h>
+#include <math.h>
+#define GL_GLEXT_PROTOTYPES
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+// Function Prototypes
+void display();
+void specialKeys();
+// Global Variables
+double rotate_y=0; 
+double rotate_x=0;
+int unit = 1.0;
+
+void display(){
+
+//  Clear screen and Z-buffer
+glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+
+
+// Multi-colored side - FRONT
+// glBegin(GL_POLYGON);
+
+// Vertices will be added in the next step
+// glEnd();
+
+
+/*
+glBegin(GL_POLYGON);
+glVertex3f( -1*unit, -1*unit, -1*unit);       // P1
+glVertex3f( -1*unit,  1*unit, -1*unit);       // P2
+glVertex3f(  1*unit,  1*unit, -1*unit);       // P3
+glVertex3f(  1*unit, -1*unit, -1*unit);       // P4
+
+glEnd();
+*/
+
+glMatrixMode(GL_MODELVIEW);
+ 
+// Reset transformations
+glLoadIdentity();
+// gluLookAt(0, 0, 0, 0, -1, 0, 0, 1, 0);
+
+// Rotate when user changes rotate_x and rotate_y
+glRotatef( rotate_x, 1.0, 0.0, 0.0 );
+glRotatef( rotate_y, 0.0, 1.0, 0.0 );
+
+
+//Multi-colored side - FRONT
+/*
+glBegin(GL_POLYGON);
+
+glColor3f( 1.0, 0.0, 0.0 );     glVertex3f(  1*unit, -1*unit, -1*unit );      // P1 is red
+glColor3f( 0.0, 1.0, 0.0 );     glVertex3f(  1*unit,  1*unit, -1*unit );      // P2 is green
+glColor3f( 0.0, 0.0, 1.0 );     glVertex3f( -1*unit,  1*unit, -1*unit );      // P3 is blue
+glColor3f( 1.0, 0.0, 1.0 );     glVertex3f( -1*unit, -1*unit, -1*unit );      // P4 is purple
+
+glEnd();
+*/
+
+// White side - BACK
+glBegin(GL_POLYGON);
+glColor3f(   1.0,  1.0, 1.0 );
+glVertex3f(  1*unit, -1*unit, 1*unit );
+glVertex3f(  1*unit,  1*unit, 1*unit );
+glVertex3f( -1*unit,  1*unit, 1*unit );
+glVertex3f( -1*unit, -1*unit, 1*unit );
+glEnd();
+
+// Purple side - RIGHT
+glBegin(GL_POLYGON);
+glColor3f(  1.0,  0.0,  1.0 );
+glVertex3f( 1*unit, -1*unit, -1*unit );
+glVertex3f( 1*unit,  1*unit, -1*unit );
+glVertex3f( 1*unit,  1*unit,  1*unit );
+glVertex3f( 1*unit, -1*unit,  1*unit );
+glEnd();
+
+// Green side - LEFT
+glBegin(GL_POLYGON);
+glColor3f(   0.0,  1.0,  0.0 );
+glVertex3f( -1*unit, -1*unit,  1*unit );
+glVertex3f( -1*unit,  1*unit,  1*unit );
+glVertex3f( -1*unit,  1*unit, -1*unit );
+glVertex3f( -1*unit, -1*unit, -1*unit );
+glEnd();
+
+// Blue side - TOP
+glBegin(GL_POLYGON);
+glColor3f(   0.0,  0.0,  1.0 );
+glVertex3f(  1,  1,  1 );
+glVertex3f(  1*unit,  1*unit, -1*unit );
+glVertex3f( -1*unit,  1*unit, -1*unit );
+glVertex3f( -1*unit,  1*unit,  1*unit );
+glEnd();
+
+// Red side - BOTTOM
+glBegin(GL_POLYGON);
+glColor3f(   1.0,  0.0,  0.0 );
+glVertex3f(  1*unit, -1*unit, -1*unit );
+glVertex3f(  1*unit, -1*unit,  1*unit );
+glVertex3f( -1*unit, -1*unit,  1*unit );
+glVertex3f( -1*unit, -1*unit, -1*unit );
+glEnd();
+
+
+// Other transformations
+glTranslatef( 0.1, 0.0, 0.0 );
+glRotatef( 180, 0.0, 1.0, 0.0 );
+glScalef( 2.0, 2.0, 0.0 );
+
+
+glFlush();
+glutSwapBuffers();
+}
+
+void specialKeys( int key, int x, int y ) {
+
+//  Right arrow - increase rotation by 5 degree
+if (key == GLUT_KEY_RIGHT)
+  rotate_y += 5;
+  
+//  Left arrow - decrease rotation by 5 degree
+else if (key == GLUT_KEY_LEFT)
+  rotate_y -= 5;
+
+else if (key == GLUT_KEY_UP)
+  rotate_x += 5;
+
+else if (key == GLUT_KEY_DOWN)
+  rotate_x -= 5;
+  
+//  Request display update
+glutPostRedisplay();
+}
+
+int main(int argc, char* argv[]){
+
+//  Initialize GLUT and process user parameters
+glutInit(&argc,argv);
+  
+//  Request double buffered true color window with Z-buffer
+glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+
+// Create window
+glutCreateWindow("Awesome Cube");
+
+//  Enable Z-buffer depth test
+glEnable(GL_DEPTH_TEST);
+
+// Callback functions
+glutDisplayFunc(display);
+glutSpecialFunc(specialKeys);
+
+//  Pass control to GLUT for events
+glutMainLoop();
+
+//  Return to OS
+return 0;
+
+}
+
