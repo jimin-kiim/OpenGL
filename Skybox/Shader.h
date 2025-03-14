79af3e069118834ef182fa9aef4d66ea0d1da2f8
jimin kim <0305jimin@gmail.com>
2025-03-14 12:04:36 +0900

    *.h and main.cpp: resolving glew.h: No such file for directory error

--- Skybox/Shader.h
+++ Skybox/Shader.h
@@ -5,11 +5,11 @@
 #include <iostream>
 #include <fstream>
 
-#include <GL\glew.h>
+#include <GL/glew.h>
 
-#include <glm\glm.hpp>
-#include <glm\gtc\matrix_transform.hpp>
-#include <glm\gtc\type_ptr.hpp>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 #include "CommonValues.h"
 
